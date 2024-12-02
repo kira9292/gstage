805159ed69ccc1e542d89868e5f1e9378b03ec90
kira <lyak@ept.sn>
2024-12-02 08:08:49 -0800

    admin

--- gStage-backend/src/main/java/sn/sonatel/dsi/ins/imoc/controller/StagiaireContratController.java
+++ gStage-backend/src/main/java/sn/sonatel/dsi/ins/imoc/controller/StagiaireContratController.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sn.sonatel.dsi.ins.imoc.domain.*;
+import sn.sonatel.dsi.ins.imoc.repository.AttestationFinStageRepository;
+import sn.sonatel.dsi.ins.imoc.repository.AttestationPresenceRepository;
 import sn.sonatel.dsi.ins.imoc.repository.CandidatRepository;
 import sn.sonatel.dsi.ins.imoc.repository.ContratRepository;
-import sn.sonatel.dsi.ins.imoc.service.StagiaireService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StagiaireContratController {
@@ -21,6 +23,11 @@ public class StagiaireContratController {
     @Autowired
     private CandidatRepository candidatRepository;
 
+    @Autowired
+    private AttestationPresenceRepository attestationPresenceRepository;
+
+    @Autowired
+    private AttestationFinStageRepository attestationFinStageRepository;
 
     @GetMapping("/api/stagiaire/contrat")
     public Contrat getContrat(){
@@ -29,12 +36,30 @@ public class StagiaireContratController {
 
         Candidat ca = this.candidatRepository.findByAppUser(user);
 
-        Contrat contrat = this.contratRepository.findByCandidat(ca);
+        List<Contrat> contrats = this.contratRepository.findByCandidat(ca);
 
-        return contrat ;
+        return (contrats.isEmpty()) ? null : contrats.get(contrats.size()-1);
     }
 
+    @GetMapping("/api/stagiare/attestation-de-presence")
+    public List<AttestationPresence> getAttestationPresence(){
+        AppUser user = (AppUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal() ;
+        Candidat ca = this.candidatRepository.findByAppUser(user);
 
+       List<AttestationPresence>  attestationPresence = this.attestationPresenceRepository.findByCandidat(ca);
+
+        return attestationPresence;
+    }
+
+    @GetMapping("/api/stagiare/attestation-de-finstage")
+    public Optional<AttestationFinStage> getAttestationFinStage(){
+        AppUser user = (AppUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal() ;
+        Candidat ca = this.candidatRepository.findByAppUser(user);
+
+        AttestationFinStage fs = ca.getAttestationFinStage();
+
+        return this.attestationFinStageRepository.findById(fs.getId());
+    }
 
 
 
